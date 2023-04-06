db1f259a0103ea39da1d4c5174bdbd90be7f67b8
leofab <leofabr3@gmail.com>
2023-04-06 17:13:10 -0300

    floatefooter component implement

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Topbar from './components/Topbar.vue';
 import SideBar from './components/SideBar.vue';
 import Body from "./components/Body.vue";
+import FloaterFooter from "./components/FloaterFooter.vue";
 </script>
 
 <template>
   <Topbar />
   <SideBar />
   <Body />
+  <FloaterFooter />
 </template>
---
+++ src/components/FloaterFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="floating-footer">
+        <p>© 2023 Chess VMSP. All rights reserved.</p>
+    </div>
+</template>
+
+<style scoped>
+.floating-footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #2f2f2f;
+    color: #fff;
+    padding: 20px;
+    box-shadow: 0 -2px 4px rgba(231, 229, 229, 0.1);
+    text-align: center;
+    font-size: 14px;
+}
+</style>
