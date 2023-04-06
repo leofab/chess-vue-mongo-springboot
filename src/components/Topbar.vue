a950260c42dbb67d6025d45c5a30c5e1a2c72d30
leofab <leofabr3@gmail.com>
2023-04-06 09:35:21 -0300

    topbaralt firsttry

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="topbar">
-      <div class="topbar__logo">My Site</div>
-      <nav class="topbar__nav">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
+      <div class="topbar__logo"></div>
+        <nav class="topbar__nav">
+            <ul>
+              <li><a href="#">Chess VMSP</a></li>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class="right_nav">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Register</a></li>
+            </ul>
+          </nav>                 
     </div>
   </template>
   
@@ -18,32 +23,37 @@
   top: 0;
   left: 0;
   right: 0;
-  background-color: #000;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #2f2f2f;
+  box-shadow: 0 2px 4px rgba(231, 229, 229, 0.1);
   padding: 10px;
   display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
 }
-
-.topbar_logo a {
-  color: #333;
+.topbar__logo {
+  color: #8c8b8b;
   text-decoration: none;
   font-size: 1.2rem;
-  margin-right: 1rem;
+  padding-right: 1em;
 }
 
-.topbar_nav a:hover {
+
+.topbar__nav {
+  display: flex;
+}
+
+.topbar__nav a:hover {
   width: 2em;
   color: #555;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
   flex: 1;
 }
 .topbar__nav ul {
   list-style: none;
   margin: 0;
-  padding: 0;
   display: flex;
+}
+.topbar__nav ul:first-of-type {
+  margin-left: auto;
 }
 
 .topbar__nav li {
