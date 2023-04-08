1f0299e9e938cbd6744294024188f48670938e88
leofab <leofabr3@gmail.com>
2023-04-07 23:07:54 -0300

    resolving vue-router inconsistency

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '@/views/Home.vue';
+import App from '@/App.vue';
 import Login from '@/components/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
+        name: 'App',
+        component: App,
     },
     {
         path: '/login',
