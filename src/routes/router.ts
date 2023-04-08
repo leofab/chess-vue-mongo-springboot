94c1af02cd920aac1283613c8afb0e2ea64bf740
leofab <leofabr3@gmail.com>
2023-04-07 22:59:57 -0300

    resolving path inconsistencies in tsconfig.json about @ and ./src routing

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue';
-import Login from './components';
+import Login from '@/components/Login.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
