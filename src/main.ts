407579174062bd0f6600e9e8aad54c3982d89043
leofab <leofabr3@gmail.com>
2023-04-09 10:48:40 -0300

    vuegoogleoauth2 deprecated add new vue3googlelogin with google identity

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "@/routes/router";
+import client_secret from "../secret/client_secret.json"
+// @ts-ignore
+import gAuthClient from "vue3-google-oauth2";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(gAuthClient, {
+    clientId: client_secret.web.client_id,
+    scope: 'email',
+    prompt: 'consent'
+})
 
 app.mount('#app')
