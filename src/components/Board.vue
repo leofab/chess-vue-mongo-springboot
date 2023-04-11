60f4723bae6bfe36ead51076e00445a6e7acab0f
leofab <leofabr3@gmail.com>
2023-04-11 16:29:11 -0300

    board svg in play view

---
+++ src/components/Board.vue
@@ -0,0 +1,20 @@
+<script lang="js">
+import {defineComponent} from "vue";
+import board from "/public/Chess_Board.svg"
+
+export default defineComponent({
+    components: {board}
+})
+</script>
+<template>
+    <img src="/Chess_Board.svg" alt="Board" class="board">
+</template>
+
+<style scoped>
+.board{
+    position: relative;
+    top:4.2rem;
+    left: 24rem;
+    width: 52rem;
+}
+</style>
---
+++ src/view/Play.vue
@@ -0,0 +1,19 @@
+<script lang="js">
+
+import {defineComponent} from "vue";
+import UserSideBarHome from "@/components/UserSideBarHome.vue";
+import Body from "@/components/Body.vue";
+import Board from "@/components/Board.vue"
+
+export default defineComponent({
+    components: {Body, UserSideBarHome, Board}
+})
+</script>
+<template>
+  <div>
+      <div class="container">
+          <UserSideBarHome />
+          <Board />
+      </div>
+  </div>
+</template>
