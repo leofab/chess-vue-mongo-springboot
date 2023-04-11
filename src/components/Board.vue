9e2acec40580123737419e890a9f4c26772cbeba
leofab <leofabr3@gmail.com>
2023-04-11 19:24:40 -0300

    board complete

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 <template>
-    <BoardSVG class="board" />
+<!--    <BoardSVG class="board" />-->
 
 
     <Square class="square_tw_18"/>
@@ -22,6 +22,69 @@ export default {
     <Square class="square_tw_13"/>
     <Square class="square_tw_12"/>
     <Square class="square_tw_11"/>
+
+    <Square class="square_tw_28"/>
+    <Square class="square_tw_27"/>
+    <Square class="square_tw_26"/>
+    <Square class="square_tw_25"/>
+    <Square class="square_tw_24"/>
+    <Square class="square_tw_23"/>
+    <Square class="square_tw_22"/>
+    <Square class="square_tw_21"/>
+
+    <Square class="square_tw_38"/>
+    <Square class="square_tw_37"/>
+    <Square class="square_tw_36"/>
+    <Square class="square_tw_35"/>
+    <Square class="square_tw_34"/>
+    <Square class="square_tw_33"/>
+    <Square class="square_tw_32"/>
+    <Square class="square_tw_31"/>
+
+    <Square class="square_tw_48"/>
+    <Square class="square_tw_47"/>
+    <Square class="square_tw_46"/>
+    <Square class="square_tw_45"/>
+    <Square class="square_tw_44"/>
+    <Square class="square_tw_43"/>
+    <Square class="square_tw_42"/>
+    <Square class="square_tw_41"/>
+
+    <Square class="square_tw_58"/>
+    <Square class="square_tw_57"/>
+    <Square class="square_tw_56"/>
+    <Square class="square_tw_55"/>
+    <Square class="square_tw_54"/>
+    <Square class="square_tw_53"/>
+    <Square class="square_tw_52"/>
+    <Square class="square_tw_51"/>
+
+    <Square class="square_tw_68"/>
+    <Square class="square_tw_67"/>
+    <Square class="square_tw_66"/>
+    <Square class="square_tw_65"/>
+    <Square class="square_tw_64"/>
+    <Square class="square_tw_63"/>
+    <Square class="square_tw_62"/>
+    <Square class="square_tw_61"/>
+
+    <Square class="square_tw_78"/>
+    <Square class="square_tw_77"/>
+    <Square class="square_tw_76"/>
+    <Square class="square_tw_75"/>
+    <Square class="square_tw_74"/>
+    <Square class="square_tw_73"/>
+    <Square class="square_tw_72"/>
+    <Square class="square_tw_71"/>
+
+    <Square class="square_tw_88"/>
+    <Square class="square_tw_87"/>
+    <Square class="square_tw_86"/>
+    <Square class="square_tw_85"/>
+    <Square class="square_tw_84"/>
+    <Square class="square_tw_83"/>
+    <Square class="square_tw_82"/>
+    <Square class="square_tw_81"/>
 
 
 </template>
@@ -90,5 +153,397 @@ export default {
     position: fixed;
     left: 199px;
     top:517px;
+}
+.square_tw_28{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:453px;
+}
+.square_tw_27{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 583px;
+    top:453px;
+}
+.square_tw_26{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:453px;
+}
+.square_tw_25{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:453px;
+}
+.square_tw_24{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:453px;
+}
+.square_tw_23{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:453px;
+}
+.square_tw_22{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:453px;
+}
+.square_tw_21{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:453px;
+}
+.square_tw_38{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:389px;
+}
+.square_tw_37{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 583px;
+    top:389px;
+}
+.square_tw_36{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:389px;
+}
+.square_tw_35{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:389px;
+}
+.square_tw_34{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:389px;
+}
+.square_tw_33{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:389px;
+}
+.square_tw_32{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:389px;
+}
+.square_tw_31{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:389px;
+}
+.square_tw_48{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:325px;
+}
+.square_tw_47  {
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 325px;
+    top:325px;
+}
+.square_tw_46{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:325px;
+}
+.square_tw_45{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:325px;
+}
+.square_tw_44{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:325px;
+}
+.square_tw_43{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:325px;
+}
+.square_tw_42{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:325px;
+}
+.square_tw_41{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:325px;
+}
+.square_tw_58{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:261px;
+}
+.square_tw_57{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 325px;
+    top:261px;
+}
+.square_tw_56{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:261px;
+}
+.square_tw_55{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:261px;
+}
+.square_tw_54{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:261px;
+}
+.square_tw_53{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:261px;
+}
+.square_tw_52{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:261px;
+}
+.square_tw_51{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:261px;
+}
+.square_tw_68{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:197px;
+}
+.square_tw_67{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 325px;
+    top:197px;
+}
+.square_tw_66{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:197px;
+}
+.square_tw_65{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:197px;
+}
+.square_tw_64{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:197px;
+}
+.square_tw_63{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:197px;
+}
+.square_tw_62{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:197px;
+}
+.square_tw_61{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:197px;
+}
+.square_tw_78{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:133px;
+}
+.square_tw_77{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 325px;
+    top:133px;
+}
+.square_tw_76{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:133px;
+}
+.square_tw_75{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:133px;
+}
+.square_tw_74{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:133px;
+}
+.square_tw_73{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:133px;
+}
+.square_tw_72{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:133px;
+}
+.square_tw_71{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:133px;
+}
+.square_tw_88{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 647px;
+    top:69px;
+}
+.square_tw_87{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 325px;
+    top:69px;
+}
+.square_tw_86{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 519px;
+    top:69px;
+}
+.square_tw_85{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 455px;
+    top:69px;
+}
+.square_tw_84{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 391px;
+    top:69px;
+}
+.square_tw_83{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 327px;
+    top:69px;
+}
+.square_tw_82{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 263px;
+    top:69px;
+}
+.square_tw_81{
+    width: 90px;
+    height: 90px;
+    position: fixed;
+    left: 199px;
+    top:69px;
 }
 </style>
