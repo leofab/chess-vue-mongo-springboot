d807182772603b23cfc5051730435d83f2217831
leofab <leofabr3@gmail.com>
2023-04-11 19:48:58 -0300

    classes square refact

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -15,76 +15,76 @@ export default {
 
 
     <Square class="square_tw_18"/>
-    <Square class="square_tw_17"/>
-    <Square class="square_tw_16"/>
-    <Square class="square_tw_15"/>
-    <Square class="square_tw_14"/>
-    <Square class="square_tw_13"/>
-    <Square class="square_tw_12"/>
+    <Square class="square_nw_17"/>
+    <Square class="square_bw_16"/>
+    <Square class="square_kw_15"/>
+    <Square class="square_qw_14"/>
+    <Square class="square_bw_13"/>
+    <Square class="square_nw_12"/>
     <Square class="square_tw_11"/>
 
-    <Square class="square_tw_28"/>
-    <Square class="square_tw_27"/>
-    <Square class="square_tw_26"/>
-    <Square class="square_tw_25"/>
-    <Square class="square_tw_24"/>
-    <Square class="square_tw_23"/>
-    <Square class="square_tw_22"/>
-    <Square class="square_tw_21"/>
+    <Square class="square_pw_28"/>
+    <Square class="square_pw_27"/>
+    <Square class="square_pw_26"/>
+    <Square class="square_pw_25"/>
+    <Square class="square_pw_24"/>
+    <Square class="square_pw_23"/>
+    <Square class="square_pw_22"/>
+    <Square class="square_pw_21"/>
 
-    <Square class="square_tw_38"/>
-    <Square class="square_tw_37"/>
-    <Square class="square_tw_36"/>
-    <Square class="square_tw_35"/>
-    <Square class="square_tw_34"/>
-    <Square class="square_tw_33"/>
-    <Square class="square_tw_32"/>
-    <Square class="square_tw_31"/>
+    <Square class="square__38"/>
+    <Square class="square__37"/>
+    <Square class="square__36"/>
+    <Square class="square__35"/>
+    <Square class="square__34"/>
+    <Square class="square__33"/>
+    <Square class="square__32"/>
+    <Square class="square__31"/>
 
-    <Square class="square_tw_48"/>
-    <Square class="square_tw_47"/>
-    <Square class="square_tw_46"/>
-    <Square class="square_tw_45"/>
-    <Square class="square_tw_44"/>
-    <Square class="square_tw_43"/>
-    <Square class="square_tw_42"/>
-    <Square class="square_tw_41"/>
+    <Square class="square__48"/>
+    <Square class="square__47"/>
+    <Square class="square__46"/>
+    <Square class="square__45"/>
+    <Square class="square__44"/>
+    <Square class="square__43"/>
+    <Square class="square__42"/>
+    <Square class="square__41"/>
 
-    <Square class="square_tw_58"/>
-    <Square class="square_tw_57"/>
-    <Square class="square_tw_56"/>
-    <Square class="square_tw_55"/>
-    <Square class="square_tw_54"/>
-    <Square class="square_tw_53"/>
-    <Square class="square_tw_52"/>
-    <Square class="square_tw_51"/>
+    <Square class="square__58"/>
+    <Square class="square__57"/>
+    <Square class="square__56"/>
+    <Square class="square__55"/>
+    <Square class="square__54"/>
+    <Square class="square__53"/>
+    <Square class="square__52"/>
+    <Square class="square__51"/>
 
-    <Square class="square_tw_68"/>
-    <Square class="square_tw_67"/>
-    <Square class="square_tw_66"/>
-    <Square class="square_tw_65"/>
-    <Square class="square_tw_64"/>
-    <Square class="square_tw_63"/>
-    <Square class="square_tw_62"/>
-    <Square class="square_tw_61"/>
+    <Square class="square__68"/>
+    <Square class="square__67"/>
+    <Square class="square__66"/>
+    <Square class="square__65"/>
+    <Square class="square__64"/>
+    <Square class="square__63"/>
+    <Square class="square__62"/>
+    <Square class="square__61"/>
 
-    <Square class="square_tw_78"/>
-    <Square class="square_tw_77"/>
-    <Square class="square_tw_76"/>
-    <Square class="square_tw_75"/>
-    <Square class="square_tw_74"/>
-    <Square class="square_tw_73"/>
-    <Square class="square_tw_72"/>
-    <Square class="square_tw_71"/>
+    <Square class="square_pb_78"/>
+    <Square class="square_pb_77"/>
+    <Square class="square_pb_76"/>
+    <Square class="square_pb_75"/>
+    <Square class="square_pb_74"/>
+    <Square class="square_pb_73"/>
+    <Square class="square_pb_72"/>
+    <Square class="square_pb_71"/>
 
-    <Square class="square_tw_88"/>
-    <Square class="square_tw_87"/>
-    <Square class="square_tw_86"/>
-    <Square class="square_tw_85"/>
-    <Square class="square_tw_84"/>
-    <Square class="square_tw_83"/>
-    <Square class="square_tw_82"/>
-    <Square class="square_tw_81"/>
+    <Square class="square_tb_88"/>
+    <Square class="square_nb_87"/>
+    <Square class="square_bb_86"/>
+    <Square class="square_qb_85"/>
+    <Square class="square_kb_84"/>
+    <Square class="square_bb_83"/>
+    <Square class="square_nb_82"/>
+    <Square class="square_tb_81"/>
 
 
 </template>
@@ -105,42 +105,42 @@ export default {
     left: 647px;
     top:517px;
 }
-.square_tw_17{
+.square_nw_17{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:517px;
 }
-.square_tw_16{
+.square_bw_16{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:517px;
 }
-.square_tw_15{
+.square_kw_15{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:517px;
 }
-.square_tw_14{
+.square_qw_14{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:517px;
 }
-.square_tw_13{
+.square_bw_13{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:517px;
 }
-.square_tw_12{
+.square_nw_12{
     width: 90px;
     height: 90px;
     position: fixed;
@@ -154,392 +154,392 @@ export default {
     left: 199px;
     top:517px;
 }
-.square_tw_28{
+.square_pw_28{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:453px;
 }
-.square_tw_27{
+.square_pw_27{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:453px;
 }
-.square_tw_26{
+.square_pw_26{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:453px;
 }
-.square_tw_25{
+.square_pw_25{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:453px;
 }
-.square_tw_24{
+.square_pw_24{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:453px;
 }
-.square_tw_23{
+.square_pw_23{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:453px;
 }
-.square_tw_22{
+.square_pw_22{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:453px;
 }
-.square_tw_21{
+.square_pw_21{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:453px;
 }
-.square_tw_38{
+.square__38{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:389px;
 }
-.square_tw_37{
+.square__37{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:389px;
 }
-.square_tw_36{
+.square__36{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:389px;
 }
-.square_tw_35{
+.square__35{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:389px;
 }
-.square_tw_34{
+.square__34{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:389px;
 }
-.square_tw_33{
+.square__33{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:389px;
 }
-.square_tw_32{
+.square__32{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:389px;
 }
-.square_tw_31{
+.square__31{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:389px;
 }
-.square_tw_48{
+.square__48{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:325px;
 }
-.square_tw_47  {
+.square__47  {
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:325px;
 }
-.square_tw_46{
+.square__46{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:325px;
 }
-.square_tw_45{
+.square__45{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:325px;
 }
-.square_tw_44{
+.square__44{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:325px;
 }
-.square_tw_43{
+.square__43{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:325px;
 }
-.square_tw_42{
+.square__42{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:325px;
 }
-.square_tw_41{
+.square__41{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:325px;
 }
-.square_tw_58{
+.square__58{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:261px;
 }
-.square_tw_57{
+.square__57{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:261px;
 }
-.square_tw_56{
+.square__56{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:261px;
 }
-.square_tw_55{
+.square__55{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:261px;
 }
-.square_tw_54{
+.square__54{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:261px;
 }
-.square_tw_53{
+.square__53{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:261px;
 }
-.square_tw_52{
+.square__52{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:261px;
 }
-.square_tw_51{
+.square__51{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:261px;
 }
-.square_tw_68{
+.square__68{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:197px;
 }
-.square_tw_67{
+.square__67{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:197px;
 }
-.square_tw_66{
+.square__66{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:197px;
 }
-.square_tw_65{
+.square__65{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:197px;
 }
-.square_tw_64{
+.square__64{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:197px;
 }
-.square_tw_63{
+.square__63{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:197px;
 }
-.square_tw_62{
+.square__62{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:197px;
 }
-.square_tw_61{
+.square__61{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:197px;
 }
-.square_tw_78{
+.square_pb_78{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:133px;
 }
-.square_tw_77{
+.square_pb_77{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:133px;
 }
-.square_tw_76{
+.square_pb_76{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:133px;
 }
-.square_tw_75{
+.square_pb_75{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:133px;
 }
-.square_tw_74{
+.square_pb_74{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:133px;
 }
-.square_tw_73{
+.square_pb_73{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:133px;
 }
-.square_tw_72{
+.square_pb_72{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:133px;
 }
-.square_tw_71{
+.square_pb_71{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 199px;
     top:133px;
 }
-.square_tw_88{
+.square_tb_88{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 647px;
     top:69px;
 }
-.square_tw_87{
+.square_nb_87{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 583px;
     top:69px;
 }
-.square_tw_86{
+.square_bb_86{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 519px;
     top:69px;
 }
-.square_tw_85{
+.square_qb_85{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:69px;
 }
-.square_tw_84{
+.square_kb_84{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 391px;
     top:69px;
 }
-.square_tw_83{
+.square_bb_83{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 327px;
     top:69px;
 }
-.square_tw_82{
+.square_nb_82{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 263px;
     top:69px;
 }
-.square_tw_81{
+.square_tb_81{
     width: 90px;
     height: 90px;
     position: fixed;
