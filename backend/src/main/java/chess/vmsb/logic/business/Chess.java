eac52ad38c31a7c1b11244aa8c247a85858e417a
leofab <leofabr3@gmail.com>
2023-04-13 11:38:36 -0300

    Chess main with Board and Players passed

--- backend/src/main/java/chess/vmsb/logic/business/Chess.java
+++ backend/src/main/java/chess/vmsb/logic/business/Chess.java
@@ -8,6 +8,11 @@ public class Chess extends ManagePlayerTurn {
 
     public static void main(String[] args) {
 
+        Player playerW = new Player("joao",true);
+        Player playerB = new Player("Maria",false);
+
+        Board board = new Board(playerW,playerB);
+
     }
 
     public void setUserUI(UserInterface userUI) {
