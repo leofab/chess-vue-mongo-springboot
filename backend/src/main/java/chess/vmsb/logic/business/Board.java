d4e869ea886d2de8d1932a6e23e4656558069aed
leofab <leofabr3@gmail.com>
2023-04-13 11:05:31 -0300

    UserInterface implement

--- backend/src/main/java/chess/vmsb/logic/business/Board.java
+++ backend/src/main/java/chess/vmsb/logic/business/Board.java
@@ -19,12 +19,6 @@ public class Board {
 
         this.gameBoard = b.gameBoard;
 
-    }public Board(Board b, Player playerW, Player playerB){
-
-        this.gameBoard = b.gameBoard;
-        this.playerW = playerW;
-        this.playerB = playerB;
-
     }
 
     public Square[][] getGameBoard() {
