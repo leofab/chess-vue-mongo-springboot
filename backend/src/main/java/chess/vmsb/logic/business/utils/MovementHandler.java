6752b442f6fad2a7c49cb348c0dc344ecb991d6d
leofab <leofab3@gmail.com>
2023-05-08 13:07:46 -0300

    getKingXY code refact for less complexity

--- backend/src/main/java/chess/vmsb/logic/business/utils/MovementHandler.java
+++ backend/src/main/java/chess/vmsb/logic/business/utils/MovementHandler.java
@@ -7,32 +7,28 @@ import chess.vmsb.logic.business.models.Player;
 import java.util.ArrayList;
 
 public class MovementHandler {
-  private int countRep;
-  private int pieceCheckCoord;
+  private int countRep = 0;
+  private int[] pieceCheckCoord={-1,-1};
   //TODO talks to VUE
-//    private UI userUI;
 
-  public int[] getKingXY(Board board, Player[] player) {
-    Player playerW = player[0];
-    Player playerB = player[1];
-    int[] wKingXY = new int[2];
-    int[] bKingXY = new int[2];
-    int[] wbKingXY = new int[4];
-    Piece wKing = playerW.getPieces().stream().filter(piece -> piece.getPieceSign() == 'k').findFirst().get();
-    Piece bKing = playerB.getPieces().stream().filter(piece -> piece.getPieceSign() == 'K').findFirst().get();
-    for (int i = 0; i < board.getGameBoard().length; i++) {
-      for (int j = 0; j < board.getGameBoard()[i].length; j++) {
-        if (board.getGameBoard()[i][j].getPiece() == wKing) {
-          wbKingXY[0] = i;
-          wbKingXY[1] = j;
-        }
-        if (board.getGameBoard()[i][j].getPiece() == bKing) {
-          wbKingXY[2] = i;
-          wbKingXY[3] = j;
+  public int[] getKingXY(Board board, Player[] player, int whichPlayer) {
+    int[] kingPos = new int[2];
+    char cmp;
+    for(int i=0;i<8;i++){
+      for(int j=0;j<8;j++){
+        if(board.getGameBoard()[i][j].getPiece()==null)continue;
+        if(board.getGameBoard()[i][j].getPiece().getClass().toString().equals("class data.King")){
+          cmp=board.getGameBoard()[i][j].getPiece().getPieceSign();
+          int pos=(cmp=='k')?0:1;
+          if(pos==whichPlayer){
+            kingPos[0]=i;
+            kingPos[1]=j;
+            break;
+          }
         }
       }
     }
-    return wbKingXY;
+    return kingPos;
   }
 
   static boolean isKingFrom(Board board,ArrayList<ArrayList<Integer>> moveData) {
@@ -78,49 +74,31 @@ public class MovementHandler {
 
   public static Object[] performCastling(Board board, Player[] player, ArrayList<ArrayList<Integer>> moveData) {
     //need to return object and players
-
     int from[]=Functional.splitDataPair(moveData.get(0));//row,col
     int to[]=Functional.splitDataPair(moveData.get(1));//row,col
 
-    if(board.getGameBoard()[to[0]][to[1]].getPiece()!=null){// to add piece to cemetery
-      Piece toDelete=board.getGameBoard()[to[0]][to[1]].getPiece();
-      toDelete.setMoved(false);//in order to reboot it to initial state and perform .remove later
-      if(Character.isLowerCase(toDelete.getPieceSign())){
-        player[0].addPieceCemetery(toDelete);
-        player[0].getPieces().remove(toDelete);// only removes first occurrence of the piece
-      }else{
-        player[1].addPieceCemetery(toDelete);
-        player[1].getPieces().remove(toDelete);// only removes first occurrence of the piece
-      }
-    }
+    Piece king = board.getGameBoard()[from[0]][from[1]].getPiece();
+    Piece rook = board.getGameBoard()[to[0]][to[1]].getPiece();
+    int who=(Character.isLowerCase(king.getPieceSign()))?0:1;
 
-    Piece toSet=board.getGameBoard()[from[0]][from[1]].getPiece();
-    Piece originalPiece=toSet;
+    board.getGameBoard()[from[0]][from[1]].setPiece(null);//clear unused pieces
+    board.getGameBoard()[to[0]][to[1]].setPiece(null);
 
-    //PawnPromotion
-    char cmp=toSet.getPieceSign();
-    int pos=(cmp=='p')?0:(cmp=='P')?7:-1;
-    int who=(Character.isLowerCase(cmp))?0:1;
+    king.setMoved(true);
+    rook.setMoved(true);
 
-    if ((cmp=='p' || cmp=='P') && pos==to[0]){
-      toSet.setMoved(false);
-      player[who].getPieces().remove(toSet);
-
-      toSet=UserInterface.askPromotioPiece((cmp=='p')?true:false);
-      player[who].getPieces().add(toSet);
-
-      player[who].addToHistory(from, to, board.getGameBoard()[from[0]][from[1]].getPiece(),
-          board.getGameBoard()[to[0]][to[1]].getPiece(), toSet);
+    if(Math.abs(from[1]-to[1])==3){
+      //short castling
+      board.getGameBoard()[from[0]][6].setPiece(king);
+      board.getGameBoard()[to[0]][5].setPiece(rook);
+      player[who].addToHistory("0-0");
     }else{
-      // in case there is no promotion
-      player[who].addToHistory(from, to, board.getGameBoard()[from[0]][from[1]].getPiece(),
-          board.getGameBoard()[to[0]][to[1]].getPiece(), null);
+      //long castling
+      board.getGameBoard()[from[0]][2].setPiece(king);
+      board.getGameBoard()[to[0]][3].setPiece(rook);
+      player[who].addToHistory("0-0-0");
     }
 
-
-    toSet.setMoved(true);
-    board.getGameBoard()[to[0]][to[1]].setPiece(toSet);//moves piece
-    board.getGameBoard()[from[0]][from[1]].setPiece(null);//clears square from
     Object dataReturn[]= {board,player};
 
     return dataReturn;
