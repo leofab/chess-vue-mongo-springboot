028f5e458a3190caf6a5a31085899d68a616df83
leofab <leofab3@gmail.com>
2023-05-16 12:29:28 -0300

    refacting board with right pos

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -218,23 +218,23 @@ export default {
     fill: gold;
     " class="square_tb_81"/>
 
-    <Queen style="
-    fill: gold;
-    " class="square_qb_85"/>
-
     <King style="
     fill: gold;
-    " class="square_kb_84"/>
+    " class="square_kb_85"/>
+
+    <Queen style="
+    fill: gold;
+    " class="square_qb_84"/>
 
 </template>
 
 <style scoped>
 .board{
-    top: 5rem;
-    width: 52rem;
-    height: 52rem;
-    position: relative;
-    left: 20rem;
+    top: 7rem;
+    width: 51.6rem;
+    height: 51.6rem;
+    position: fixed;
+    left: 21rem;
     opacity: 20%;
 }
 .square_tw_18{
@@ -650,14 +650,14 @@ export default {
     left: 519px;
     top:69px;
 }
-.square_qb_85{
+.square_kb_85{
     width: 90px;
     height: 90px;
     position: fixed;
     left: 455px;
     top:69px;
 }
-.square_kb_84{
+.square_qb_84{
     width: 90px;
     height: 90px;
     position: fixed;
